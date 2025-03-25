f7e11f02b10d3f95bf5e2e6a22b1b5afb71a9078
kimnamsun <nsunny0908@naver.com>
2025-03-25 22:49:23 +0900

    style: tailwindcss config 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,15 @@ const config: Config = {
       fontFamily: {
         sans: ["Galmuri11"],
       },
+      animation: {
+        float: "float 2s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-1rem)" },
+        },
+      },
     },
   },
   plugins: [],
