35d7610db21ecc0651bab56bc81c85c2f42088a7
kimnamsun <nsunny0908@naver.com>
2025-04-20 14:48:49 +0900

    refactor: 파일 구조 변경 (PokemonCanvas 제거)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,87 @@
-import { getRandomPokemon } from "./api/getPokemon";
-import PokemonCanvas from "./components/PokemonCanvas";
+"use client";
 
-const Home = async () => {
-  const pokemon = await getRandomPokemon();
+import React, { useCallback, useEffect, useState } from "react";
+import { Pokemon } from "@/@types/pokemon";
+import { Canvas } from "@react-three/fiber";
 
-  if (!pokemon) {
-    return null;
-  }
+import { getRandomPokemon } from "@/app/api/getPokemon";
 
-  return <PokemonCanvas pokemon={pokemon} />;
+const Pokeball = React.lazy(() => import("@/app/components/Pokeball"));
+const PokemonCard = React.lazy(() => import("@/app/components/PokemonCard"));
+
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+  const [showPokeball, setShowPokeball] = useState(true);
+  const [showCard, setShowCard] = useState(false);
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const data = await getRandomPokemon();
+      setPokemon(data);
+    };
+    fetchPokemon();
+  }, []);
+
+  useEffect(() => {
+    const duration = 3000;
+    const increment = 100 / (duration / 100);
+    let currentProgress = 0;
+
+    const interval = setInterval(() => {
+      currentProgress += increment;
+      if (currentProgress >= 100) {
+        setProgress(100);
+        setLoading(false);
+        clearInterval(interval);
+      } else {
+        setProgress(currentProgress);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleAnimationComplete = useCallback(() => {
+    setShowPokeball(false);
+    setShowCard(true);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <img
+            src="/loading.gif"
+            alt="loading"
+            className="h-15 w-16 sm:h-24 sm:w-24"
+          />
+          <h1 className="mb-4 mt-6 text-xl font-bold sm:text-2xl">
+            잠시만 기다려주세요...
+          </h1>
+
+          <div className="flex w-full justify-center">
+            <div className="h-3 w-full max-w-xs overflow-hidden rounded-full bg-gray-200 sm:h-4 sm:max-w-md">
+              <div
+                className="mr-2 h-full bg-green-500 transition-all duration-100 ease-linear"
+                style={{ width: `${progress}%` }}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          {showPokeball && (
+            <Canvas>
+              <Pokeball onAnimationComplete={handleAnimationComplete} />
+            </Canvas>
+          )}
+          {showCard && pokemon && <PokemonCard pokemon={pokemon} />}
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
